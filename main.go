36dbb6a99b57986c495b266ce70cb5be76a7c47f
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2024-09-15 00:08:28 +0300

    Add URL Parameter

--- main.go
+++ main.go
@@ -7,21 +7,20 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type Router struct{}
-
+// type Router struct{}
 // ServeHTTP implements http.Handler.
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/faq":
+// 		faqHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hi, Welcome to Web Dev with Golang</h1>")
@@ -34,11 +33,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>My email: <a href=\"mailto:abdullah.alkheshen@gmail.com\">abdullah.alkheshen@gmail.com</a>.</p>")
+
+	
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Here are the frequently asked questions.</p>")
+}
+
+func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	galleryID := chi.URLParam(r, "id")
+
+	w.Write([]byte(fmt.Sprintf("Gallery ID: %s", galleryID)))
 }
 
 func main() {
@@ -50,6 +57,7 @@ func main() {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	
+	r.Get("/gallery/{id}", galleryHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
