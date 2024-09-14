0f98bd254d3f3879554b92d21a46c854df728653
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2024-09-14 12:54:04 +0300

    Add Dynamic Reloading via Modd

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to Web Dev with Go</h1>")
+	fmt.Fprintf(w, "<h1>Hi, Welcome to Web Dev with Golang</h1>")
+	fmt.Fprintf(w, "<h1>I love Golang</h1>")
+	fmt.Fprintf(w, "<h1>I also love C++ & Rust</h1>")
+	fmt.Fprintf(w, "<h1>I will eventually build my own website</h1>")
+
 }
 
 func main() {
