cfc2ec40fb6df2fe46dc1fba75c9e050f947f54d
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2024-09-14 23:25:16 +0300

    Install Chi for Routing

--- main.go
+++ main.go
@@ -5,7 +5,23 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+// ServeHTTP implements http.Handler.
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hi, Welcome to Web Dev with Golang</h1>")
 	fmt.Fprintf(w, "<h1>I love Golang</h1>")
 	fmt.Fprintf(w, "<h1>I also love C++ & Rust</h1>")
@@ -13,8 +29,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>My email: <a href=\"mailto:abdullah.alkheshen@gmail.com\">abdullah.alkheshen@gmail.com</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Here are the frequently asked questions.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe("localhost:3000", nil)
+	var router Router
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", router)
 }
